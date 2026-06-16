05fe08497079f3f113aab600334329ab8950c2a2
mengkheang-seng <2025422seng@aupp.edu.kh>
2026-06-16 20:07:59 +0700

    ready to summit

--- API_Gateway/index.js
+++ API_Gateway/index.js
@@ -7,37 +7,44 @@ require('dotenv').config();
 const verifyToken = require('../shared/middleware/verifyToken');
 const authorizeRole = require('../shared/middleware/authorizeRole');
 
-// Auth routes — no token needed, just passes through
+// Auth routes — no token needed
 app.use('/auth', (req, res) => {
+  req.url = req.originalUrl;
   console.log('API Gateway → Auth Service');
   proxy.web(req, res, { target: 'http://auth_service:3000' });
 });
 
 // Book routes — member + librarian
 app.use('/books', verifyToken, authorizeRole('member', 'librarian'), (req, res) => {
+  req.url = req.originalUrl;
   console.log('API Gateway → Book Service (load balanced)');
   proxy.web(req, res, { target: 'http://nginx:8080' });
 });
 
 // Borrow routes — member only
 app.use('/borrow/borrow', verifyToken, authorizeRole('member'), (req, res) => {
+  req.url = req.originalUrl;
   proxy.web(req, res, { target: 'http://borrow_service:3002' });
 });
 
 app.use('/borrow/return', verifyToken, authorizeRole('member'), (req, res) => {
+  req.url = req.originalUrl;
   proxy.web(req, res, { target: 'http://borrow_service:3002' });
 });
 
 app.use('/borrow/history', verifyToken, authorizeRole('member'), (req, res) => {
+  req.url = req.originalUrl;
   proxy.web(req, res, { target: 'http://borrow_service:3002' });
 });
 
 // Borrow routes — librarian only
 app.use('/borrow/all', verifyToken, authorizeRole('librarian'), (req, res) => {
+  req.url = req.originalUrl;
   proxy.web(req, res, { target: 'http://borrow_service:3002' });
 });
 
 app.use('/borrow/overdue', verifyToken, authorizeRole('librarian'), (req, res) => {
+  req.url = req.originalUrl;
   proxy.web(req, res, { target: 'http://borrow_service:3002' });
 });
 
