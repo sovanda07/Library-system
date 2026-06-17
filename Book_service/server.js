635308b01d81616286156005377147b229dd4997
mengkheang-seng <2025422seng@aupp.edu.kh>
2026-06-17 19:41:14 +0700

    fix

--- Book_service/server.js
+++ Book_service/server.js
@@ -14,17 +14,3 @@ app.listen(PORT, () => {
   console.log(`Book service running on port ${PORT}`);
 });
 
-// const express = require('express');
-// const app = express();
-// const mongoose = require('mongoose');
-
-// app.use(express.json());
-
-// const dbconnect = require('./config/db.js');
-
-// const bookRouter = require('./routes/books');
-// app.use('/books', bookRouter);
-
-// app.listen(3001, () => {
-//     console.log("Book service running on port 3001");
-// })
